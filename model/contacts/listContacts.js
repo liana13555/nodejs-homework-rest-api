5d979c0802de23d4f542d0f2afe83b088d349af7
unknown <barishovaliana@gmail.com>
2021-11-22 01:08:41 +0100

    Add homework-2

---
+++ model/contacts/listContacts.js
@@ -0,0 +1,11 @@
+const fs = require('fs').promises;
+
+const contactsPath = require("./contactsPath");
+
+const listContacts = async () => {
+    const data = await fs.readFile(contactsPath, 'utf8');
+    return JSON.parse(data);
+};
+
+
+module.exports = listContacts;
