2987b350fa1542d3c10d6208f44161ae7d5b2fbb
unknown <barishovaliana@gmail.com>
2021-11-26 00:51:09 +0100

    Add homework-3

--- controllers/contacts/updateStatusContact.js
+++ controllers/contacts/updateStatusContact.js
@@ -1,9 +1,10 @@
 const { Contact } = require('../../models')
 const { NotFound } = require('http-errors')
 
-const updateContactById = async (req, res) => {
+const updateStatusContact = async (req, res) => {
   const { id } = req.params
-  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true })
+  const { favorite } = req.body
+  const result = await Contact.findByIdAndUpdate(id, { favorite }, { new: true })
   if (!result) {
     throw new NotFound(`Contact with id=${id} not found!`)
   }
@@ -16,4 +17,4 @@ const updateContactById = async (req, res) => {
   })
 }
 
-module.exports = updateContactById
+module.exports = updateStatusContact
